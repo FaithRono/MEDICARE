21091860612fb9a70e0b30e6dbed515dd7f84f65
faith rono <faithrono132@gmail.com>
2024-09-02 10:48:31 +0300

    created a medicare website that will allows users to book appointments online then talk with doctors

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return(
+    <div className="flex flex-col items-center justify-center h-screen  text-white-950">
+      <h1>hello world, I LOVE TYPESCRIPT</h1>
+      <Button>Click Me!</Button>
+    </div>
+  )
+}
